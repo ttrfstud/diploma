af4f73176023deb4010eb2a4d66e451d544dda64
ttrfstud <ttrfstud@gmail.com>
2014-03-02 11:30:12 +0300

    intermediate commit

--- src/automata/automaton_test.js
+++ src/automata/automaton_test.js
@@ -25,7 +25,70 @@ describe('automaton', function () {
 		done();
 	});
 
-	it('chunk is over line end long (from chunk start), empty object, no errors', function (done) {
+	// cm - chunk contains more bytes than current line needs to the end (e.g. 39 bytes, but the current line is already at 68)
+	// ce - chunk contains exactly as much bytes as needed to complete line (e.g. 3 bytes, and the line is already 78 bytes long - chunk should also include whitespace)
+	// cl - chunk contains less bytes than current lines needs
+	// -------
+	// se - start exact - object is started where this chunk is started (therefore, always eo)
+	// sb - start before - object is started before this chunk was started (therefore, always neo)
+	// sa - start after - object starts somewhere in the middle of this chunk (therefore, always eo)
+	// -------
+	// eo - empty object
+	// neo - not empty object
+	// -------
+	// e - automaton should report error
+	// ne - automaton should not reports error
+
+	// another classification
+	// a - line length
+	// b - chunk length
+	// sb - a starts before
+	// se - a and b start together
+	// sa - a starts after
+	// eb - a ends before
+	// ee - a and b end together
+	// ea - a ends after
+	// 1. a = b
+	// -- sb, eb (cm, sb, neo)
+	// -- sb, ee (impossible)
+	// -- sb, ea (impossible)
+	// -- se, eb (impossible)
+	// -- se, ee (ce, se, eo)
+	// -- se, ea (impossible)
+	// -- sa, eb (impossible)
+	// -- sa, ee (impossible)
+	// -- sa, ea
+	// =>
+	// a = b => sb, eb | se, ee | sa, ea
+	// 2. a < b
+	// -- sb, eb (cm, sb, neo) <--- same as a = b, sb, eb, but a sub-case!
+	// -- sb, ee (impossible)
+	// -- sb, ea (impossible)
+	// -- se, eb
+	// -- se, ee (impossible)
+	// -- se, ea (impossible)
+	// -- sa, eb
+	// -- sa, ee
+	// -- sa, ea
+	// =>
+	// a < b => sb, eb | se, eb | sa, eb | sa, ee | sa, ea
+	// 3. a > b
+	// -- sb, eb
+	// -- sb, ee
+	// -- sb, ea
+	// -- se, eb (impossible)
+	// -- se, ee (impossible)
+	// -- se, ea
+	// -- sa, eb (impossible)
+	// -- sa, ee (impossible)
+	// -- sa, ea
+	// =>
+	// a > b => sb, eb | sb, ee | sb, ea | se, ea | sa, ea
+
+	// a = b here means: |a| = 80 & b = |81|
+	// a < b here means: |a| = 79 & b = |81|
+	// a > b here means: |a| = 80 & b = |80}
+	it('#a = b, sb, eb', function (done) {
 		var object = {};
 					12345678901234567890123456789012345678901234567890123456789012345678901234567890;
 		var line = 'ATOM  16596  N   GLN C 123       8.285  -2.726 -26.326  1.00 43.96           N  \nAT';
@@ -64,7 +127,7 @@ describe('automaton', function () {
 		done();
 	});
 	
-	it('chunk is exactly to line end long (from chunk start), empty object, no errors', function (done) {
+	it('#ce - se - eo - ne', function (done) {
 		var object = {};
 					12345678901234567890123456789012345678901234567890123456789012345678901234567890;
 		var line = 'ATOM  16596  N   GLN C 123       8.285  -2.726 -26.326  1.00 43.96           N  \n';
@@ -103,7 +166,7 @@ describe('automaton', function () {
 		done();
 	});
 
-	it('chunk is less than to the line end long (from chunk start), empty object, no errors', function (done) {
+	it('#cl - se - eo - ne', function (done) {
 		var object = {};
 					12345678901234567890123456789012345678901234567890123456789012345678901234567890;
 		var line = 'ATOM  16596  N   GLN C 123       8.285  -2.726 -26.326  1.00 43.96           N  ';
@@ -170,4 +233,50 @@ describe('automaton', function () {
 
 		done();
 	});
+	// cm - sb - neo - ne
+	// ce - sb - neo - ne
+	// cl - sb - neo - ne
+
+	// cm - sa - eo - ne
+	// ce - sa - eo - ne
+	// cl
+	it('#cm - sb - neo - ne', function (done) {
+		var object = {};
+					12345678901234567890123456789012345678901234567890123456789012345678901234567890;
+		var line = 'ATOM  16596  N   GLN C 123       8.285  -2.726 -26.326  1.00 43.96           N  \n';
+		var chunk_offset = 0;
+		var line_offset = 0;
+		var auto = atom_auto;
+
+		var result = automaton(object, tc(line), chunk_offset, line_offset, auto);
+
+		result.should.eql({
+			chunk_offset: 80,
+			signal: signal.READ_LINE
+		});
+
+		object.should.eql({
+			rec: tc('A'),
+			'_': tc('TOM  ').
+					concat(tc(' ')).concat(tc(' ')).
+					concat(tc('   ')).concat(tc('          ')),
+			serial: tc('16596'),
+			atom_name: tc(' N  '),
+			alt_loc: tc(' '),
+			residue_name: tc('GLN'),
+			chain_id: tc('C'),
+			res_seq: tc(' 123'),
+			i_code: tc(' '),
+			x: tc('   8.285'),
+			y: tc('  -2.726'),
+			z: tc(' -26.326'),
+			occupancy: tc('  1.00'),
+			temp_factor: tc(' 43.96'),
+			element: tc(' N'),
+			charge: tc('  ')
+		});
+
+		done();
+	});
+
 });
